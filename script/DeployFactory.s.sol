0bd9a5454b9f3b611c6ea590e7d34c705152daed
root <root@venkat>
2024-04-25 22:09:44 +0530

    DeployFactoryMultichain

--- script/DeployFactory.s.sol
+++ script/DeployFactory.s.sol
@@ -1,26 +1,30 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
 
-import {Script} from 'forge-std/Script.sol';
-import {CatERC20Factory} from '../src/catERC20Factory.sol';
-import {BaseMultiChainDeployer} from '../lib/GeneralisedIncentives/script/BaseMultiChainDeployer.s.sol';
-
-
-contract DeployFactory is BaseMultiChainDeployer{
-
+import { Script, console } from 'forge-std/Script.sol';
+import { BaseMultiChainDeployer } from 'GeneralisedIncentives/script/BaseMultiChainDeployer.s.sol';
+import { CatERC20Factory } from '../src/catERC20Factory.sol';
+import { Create2 } from './Create2.sol';
+ 
+contract DeployFactory is BaseMultiChainDeployer {
+ 
     CatERC20Factory catERC20Factory;
-    function _deploy() public returns(CatERC20Factory){
-            catERC20Factory = new CatERC20Factory();
-        return catERC20Factory;
+    Create2 create2;
+    
+    function _deploy() public returns (CatERC20Factory) {
+        create2 = new Create2();
+        bytes32 salt = bytes32(uint256(12345)); 
+        bytes memory creationCode = type(CatERC20Factory).creationCode;
+        CatERC20Factory deployedAddress = CatERC20Factory(create2.deploy(salt, creationCode));
+        return deployedAddress;
     }
 
-    function deployToAllChains()  iter_chains(chain_list) broadcast external {
+    function deployToAllChains() iter_chains(chain_list) broadcast external {
         _deploy();
     }
 
-    
-    function deployToAllChains( string[] memory chains) iter_chains_string(chains) broadcast external {
+    function deployToAllChains(string[] memory chains) iter_chains_string(chains) broadcast external {
         _deploy();
     }
 }
