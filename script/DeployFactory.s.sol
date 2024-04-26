df7c58c256b906293fa328e05c227d26b017017d
root <root@venkat>
2024-04-26 17:25:05 +0530

    DeployFactoryMultiChain

--- script/DeployFactory.s.sol
+++ script/DeployFactory.s.sol
@@ -6,25 +6,28 @@ import { Script, console } from 'forge-std/Script.sol';
 import { BaseMultiChainDeployer } from 'GeneralisedIncentives/script/BaseMultiChainDeployer.s.sol';
 import { CatERC20Factory } from '../src/catERC20Factory.sol';
 import { Create2 } from './Create2.sol';
+import {DeployERC20} from './DeployERC20.s.sol';
  
-contract DeployFactory is BaseMultiChainDeployer {
+contract DeployFactory is BaseMultiChainDeployer,DeployERC20 {
  
-    CatERC20Factory catERC20Factory;
+    CatERC20Factory public catERC20Factory;
     Create2 create2;
+   
     
-    function _deploy() public returns (CatERC20Factory) {
+    function _deploy() public  {
         create2 = new Create2();
         bytes32 salt = bytes32(uint256(12345)); 
         bytes memory creationCode = type(CatERC20Factory).creationCode;
-        CatERC20Factory deployedAddress = CatERC20Factory(create2.deploy(salt, creationCode));
-        return deployedAddress;
+        catERC20Factory = CatERC20Factory(create2.deploy(salt, creationCode));
+        address deployedToken = getFactoryContractAddress(catERC20Factory);
     }
+
 
     function deployToAllChains() iter_chains(chain_list) broadcast external {
         _deploy();
     }
 
-    function deployToAllChains(string[] memory chains) iter_chains_string(chains) broadcast external {
+    function deployToAllChains(string[] memory chains) iter_chains_string(chains) broadcast public {
         _deploy();
     }
 }
