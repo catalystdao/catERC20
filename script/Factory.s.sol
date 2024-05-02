91875341fcc8fadab505fe0ac03b462e429baecd
Alexander <git@lindgren.xyz>
2024-05-02 18:19:29 +0200

    feat: deployment script

--- script/Factory.s.sol
+++ script/Factory.s.sol
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+
+import {StdAssertions} from "forge-std/StdAssertions.sol";
 import {Script, console2} from "forge-std/Script.sol";
 
 import { CatERC20Factory } from "src/CatERC20Factory.sol";
 
-contract DeployFactoryScript is Script {
+contract DeployFactoryScript is Script, StdAssertions {
     function setUp() public {}
 
-    function run() public {
-        vm.startBroadcast();
+    function deployFactory() public returns(CatERC20Factory fact) {
+        vm.broadcast();
 
-        new CatERC20Factory{salt: bytes32(0)}();
+        fact = new CatERC20Factory{salt: bytes32(0x674bffae0d0816cab9be2a8df93557866622de5e01b16c801d7cc4fe66d7163c)}(); // 0x000000000d53e6b5968b70Fdd3cb68B5e216cE93
+
+        assertEq(address(fact), 0x000000000d53e6b5968b70Fdd3cb68B5e216cE93);
+        console2.logAddress(address(fact));
+    }
+
+    function test(address owner, bytes12 salt) public {
+        CatERC20Factory fact = deployFactory();
+        vm.broadcast();
+
+        address tkn = fact.deployXERC20("Name", "Symbol", owner, salt);
+
+        console2.logAddress(tkn);
     }
 }
---
+++ script/mineSalt/mineSalt.s.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "forge-std/Script.sol";
+
+import {LibString} from "./LibString.sol";
+
+import { CatERC20 } from "src/CatERC20.sol";
+import { CatERC20Factory } from "src/CatERC20Factory.sol";
+
+contract MineSalt is Script {
+    using LibString for bytes;
+
+    function setUp() public {}
+
+    function mineSalt(bytes32 initCodeHash, string memory startsWith)
+        public
+        returns (bytes32 salt, address expectedAddress)
+    {
+        string[] memory args = new string[](6);
+        args[0] = "cast";
+        args[1] = "create2";
+        args[2] = "--starts-with";
+        args[3] = startsWith;
+        args[4] = "--init-code-hash";
+        args[5] = LibString.toHexStringNoPrefix(uint256(initCodeHash), 32);
+        string memory result = string(vm.ffi(args));
+
+        uint256 addressIndex = LibString.indexOf(result, "Address: ");
+        string memory addressStr = LibString.slice(result, addressIndex + 9, addressIndex + 9 + 42);
+        expectedAddress = vm.parseAddress(addressStr);
+
+        uint256 saltIndex = LibString.indexOf(result, "Salt: ");
+        string memory saltStr = LibString.slice(result, saltIndex + 6, saltIndex + 6 + 64 + 2);
+        salt = bytes32(vm.parseBytes32(saltStr));
+    }
+
+    function mineSalt(bytes32 initCodeHash, string memory startsWith, address factoryAddress, address owner)
+        public
+        returns (bytes32 salt, address expectedAddress)
+    {
+        string[] memory args = new string[](10);
+        args[0] = "cast";
+        args[1] = "create2";
+        args[2] = "--starts-with";
+        args[3] = startsWith;
+        args[4] = "--init-code-hash";
+        args[5] = LibString.toHexStringNoPrefix(uint256(initCodeHash), 32);
+        args[6] = "--caller";
+        args[7] = LibString.toHexStringChecksummed(owner);
+        args[8] = "--deployer";
+        args[9] = LibString.toHexStringChecksummed(factoryAddress);
+        string memory result = string(vm.ffi(args));
+
+        uint256 addressIndex = LibString.indexOf(result, "Address: ");
+        string memory addressStr = LibString.slice(result, addressIndex + 9, addressIndex + 9 + 42);
+        expectedAddress = vm.parseAddress(addressStr);
+
+        uint256 saltIndex = LibString.indexOf(result, "Salt: ");
+        string memory saltStr = LibString.slice(result, saltIndex + 6, saltIndex + 6 + 64 + 2);
+        salt = bytes32(vm.parseBytes32(saltStr));
+    }
+
+    function factory(string memory prefix) public {
+        bytes32 initCodeHash = keccak256(abi.encodePacked(type(CatERC20Factory).creationCode));
+        (bytes32 salt, ) = mineSalt(initCodeHash, prefix);
+
+        // DEPLOY
+        vm.startBroadcast();
+        address actualAddress = address(new CatERC20Factory{salt: bytes32(salt)}());
+        vm.stopBroadcast();
+
+        // assertEq(actualAddress, expectedAddress);
+        console2.log(actualAddress);
+        console.logBytes32(salt);
+    }
+
+    function tokenAddress(string memory prefix, address factoryAddress, string memory name, string memory symbol, address owner) public {
+        bytes32 initCodeHash = keccak256(abi.encodePacked(type(CatERC20).creationCode, abi.encode(name, symbol, factoryAddress)));
+        (bytes32 salt, address expectedAddress) = mineSalt(initCodeHash, prefix, factoryAddress, owner);
+
+        console2.log(expectedAddress);
+        console.logBytes32(salt);
+    }
+}
